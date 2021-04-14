297062955e518ffd663033f04dda115f0f6603ba
Sajjad Kazi <sajjadkazi4@gmail.com>
2021-04-14 18:44:03 -0400

    File System Created with Props Functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() { 
+  // Use conditional rendering to open and cloe the folder 
+  // From Line 8-14 all belong to props.children
+
+    return <div>
+      <Folder name='Desktop' isOpen = {false}>
+        <Folder name= 'music'>
+            <File name = 'chillBeats.mp4'/>
+            <File name = 'superPlaylist.mp4'/>
+        </Folder>
+      <File name = 'dogs.png'/>
+      <File name = 'cats.png'/>
+      </Folder>
+        
+      <Folder name='Applications'/>
+      
+      
+        </div>  
+}
+// If the condition is true, the ternary operator returns expression(1) otherwise it returns expression(2)
+const Folder = (props) => {
+  const {name , isOpen, children} = props; // destructuring the Props objs
+  
+    return <div>
+      {name}
+      <div style={{marginLeft: '17px'}}>
+      {isOpen ? children : null}
+      </div>
+      
+      </div>
+  
+}
+const File = (props) => {
+return <div>{props.name}</div>
+
 }
 
 export default App;

8650f58af1c3d85d7c30050153b9862dd8827f3b
Sajjad Kazi <sajjadkazi4@gmail.com>
2021-04-18 08:54:40 -0400

    added CSS to my App, added state to so Folders open and Close

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() { 
   // Use conditional rendering to open and cloe the folder 
   // From Line 8-14 all belong to props.children
 
     return <div>
-      <Folder name='Desktop' isOpen = {false}>
+      <Folder name='Desktop'>
         <Folder name= 'music'>
             <File name = 'chillBeats.mp4'/>
             <File name = 'superPlaylist.mp4'/>
         </Folder>
-      <File name = 'dogs.png'/>
+      <File name = 'dogs.jpeg'/>
       <File name = 'cats.png'/>
       </Folder>
         
@@ -20,11 +20,25 @@ function App() {
         </div>  
 }
 // If the condition is true, the ternary operator returns expression(1) otherwise it returns expression(2)
-const Folder = (props) => {
-  const {name , isOpen, children} = props; // destructuring the Props objs
-  
+  const Folder = (props) => {
+  const {name ,children} = props; // destructuring the Props objs
+
+  const [ isOpen, setIsOpen ] = useState(true); //Array Destrcturing - UseState returns an Array that contains 2 Elements A Boolean and a function 
+  const direction  = isOpen ? 'down': 'right';
+ 
+  const handleSubmit = () => {
+      // setIsOpen(isOpen ? false: true);  // setting the state so if isOpen is False set it to True
+      setIsOpen(!isOpen);
+      
+  }
     return <div>
+      <span onClick={handleSubmit}> 
+      <i className = "blue folder big icon"></i>
+      <i className = {`caret ${direction} icon`}> </i>
+      </span>
+
       {name}
+      
       <div style={{marginLeft: '17px'}}>
       {isOpen ? children : null}
       </div>
@@ -33,7 +47,18 @@ const Folder = (props) => {
   
 }
 const File = (props) => {
-return <div>{props.name}</div>
+const{name} = props;
+
+const fileExtension = name.split('.')[1];
+const fileIcons = {
+  mp4:'headphones',
+  jpeg: 'file image',
+  png: 'file image outline',
+};
+
+return <div> <i className = {`${fileIcons[fileExtension]} icon`}> </i>
+  {name}
+  </div>
 
 }
 
